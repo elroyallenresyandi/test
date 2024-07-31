1581166af4392cf6959b0e83ac318339df23545d
elroyallenresyandi <elroy_resyandi_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 15:01:54 +0700

    Add files via upload
    tugas bu pasha

---
+++ tugas.java
@@ -0,0 +1,13 @@
+public class tugas{
+
+    public static void main (String[] args){
+        
+        System.out.println("halo ibu pasha perkenalkan saya");
+        System.out.println("nama: elroy allen resyandi");
+        System.out.println("alamat: jalan bandara palmerah 17q21");
+        System.out.println("nomor hp: 087802066412");
+        System.out.println("sekian dari saya terimakasih");
+
+    };
+
+}
