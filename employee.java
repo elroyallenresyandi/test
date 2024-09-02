2b3f3b46dc7840cdfc21a25727ab28770df1882b
elroyallenresyandi <elroy_resyandi_33rpl@student.smktelkom-mlg.sch.id>
2024-09-02 11:46:40 +0700

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,39 @@
+
+public class employee {
+
+
+    
+String name;
+double salary;
+
+
+public employee(String n, double s) {
+    //TODO Auto-generated constructor stub
+    name = n;
+    salary = s;
+}
+    /*Constructor employee */
+
+    // method to print
+
+    
+    
+    public void Print() {
+       
+        System.out.println("name : " + name + ", salary : " + salary);
+        
+
+    
+
+        
+    }
+}
+
+
+
+    
+    
+
+
+
+
---
+++ employee2.java
@@ -0,0 +1,22 @@
+//drive class
+
+public class employee2{
+
+    public static void main(String[] args) {
+
+        employee e1 = new employee ("jane", 3500000.00);
+        employee e2 = new employee ("brandon", 2500000.00);
+        employee e3 = new employee ("anna : ", 20000000.00);
+
+        e1.Print();
+        e2.Print();
+        e3.Print();
+
+
+
+
+
+
+    }
+
+}
